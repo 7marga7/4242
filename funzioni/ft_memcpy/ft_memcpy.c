547ec748a9beb7687b40ab54a435738cc8c6cc9b
7marga7 <salvatore.margarone@gmail.com>
2025-11-25 12:55:27 +0100

    Add files via upload

--- funzioni/ft_memcpy/ft_memcpy.c
+++ funzioni/ft_memcpy/ft_memcpy.c
@@ -3,35 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smargaro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: marga <marga@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 15:26:56 by smargaro          #+#    #+#             */
-/*   Updated: 2025/11/24 15:27:03 by smargaro         ###   ########.fr       */
+/*   Updated: 2025/11/24 19:36:24 by marga            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stddef.h>
+//#include <stdio.h>
+//#include <stddef.h>
+//#include <string.h>
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
-	i = 0;
 
-	while(i < n)
+	i = 0;
+	while (i < n)
 	{
 		((char *)dest)[i] = ((char *)src)[i];
 		i++;
 	}
 	return (dest);
 }
-int main(void)
+/*int main(void)
 {
 	int	i;
-	
-	
-	char d[] = "ciao mamma";
-	char s[] = "hello";	
-		printf("mia %s\n",ft_memcpy(d, s,  5));
+
+
+	char d[] = "ciao amma";
+	char s[] = "hello di";
+		printf("mia  %s\n",ft_memcpy(d, s,  3));
+	char t[] = "ciao amma";
+	char v[] = "hello di";
+		printf("test %s\n",memcpy(t, v,  3));
 		i++;
-}
+}*/
