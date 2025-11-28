8148efe82f5a65047d5eef1a52ebeca330ed951c
7marga7 <salvatore.margarone@gmail.com>
2025-11-28 19:29:49 +0100

    Add files via upload

--- funzioni/atoi/ft_atoi.c
+++ funzioni/atoi/ft_atoi.c
@@ -6,17 +6,33 @@
 /*   By: smargaro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 17:49:37 by smargaro          #+#    #+#             */
-/*   Updated: 2025/11/28 18:55:14 by smargaro         ###   ########.fr       */
+/*   Updated: 2025/11/28 19:28:20 by smargaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+//#include <stdio.h>
 #include <stdlib.h>
+
+int	ft_convert(const char *str, int sign)
+{
+	int			i;
+	long int	temp;
+
+	i = 0;
+	temp = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		temp = (temp * 10) + (str[i] - '0');
+		i++;
+	}
+	temp = temp * sign;
+	return (temp);
+}
 
 int	ft_atoi(const char *str)
 {
-	int	sign;
-	int	i;
+	int			sign;
+	int			i;
 	long int	temp;
 
 	i = 0;
@@ -34,23 +50,14 @@ int	ft_atoi(const char *str)
 		else if (str[i] < '0' && str[i] > '9')
 			return (0);
 		else
-		{
-			while (str[i] >= '0' && str[i] <= '9')
-			{
-				temp = (temp * 10) + (str[i] - '0');
-				i++;
-			}
-			temp = temp * sign;
-			return(temp);
-		}
+			return (ft_convert(&str[i], sign));
 	}
-	temp = temp * sign;
-	return(temp);
+	return (0);
 }
 
 /*int main()
 {
-	char	s[] = "-214748348-ss ss";
+	char	s[] = "-2147348-ss ss";
 
 	printf("mia f %d\n", ft_atoi(s));
 	printf("atoi  %d\n", atoi(s));
